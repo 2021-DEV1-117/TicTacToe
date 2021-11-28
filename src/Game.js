56d91869cf532b239dbda2fa6f207e4aa6b13f14
Your Name <youremail@yourdomain.com>
2021-11-28 10:11:30 +0100

    [Test 3]  Game should render a 3X3 html table. I first render a raw HTML to assess the test.

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const Game = () => <div className="Game" />;
+const Game = () => {
+    return <div className="Game">
+
+        <table>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+
+    </div>;
+}
 
 export default Game;
--- src/Game.test.js
+++ src/Game.test.js
@@ -8,4 +8,14 @@ describe ('Game', () => {
         let wrapper = shallow(<Game/>);
         expect(wrapper.find('div.Game').length).toEqual(1);
     });
+
+    it('Game should render a table of 3 X 3 cells',() => {
+        let wrapper = shallow(<Game />);
+        expect(wrapper.find('table').length).toEqual(1);
+        expect(wrapper.find('table tr').length).toEqual(3);
+        wrapper.find('table tr').forEach((tr) => {
+            expect(tr.find('td').length).toEqual(3);
+        })
+    });
+
 });
