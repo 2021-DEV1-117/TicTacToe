d0308229481848e8eab516e406fb8b90e7243c97
Your Name <youremail@yourdomain.com>
2021-11-28 10:47:47 +0100

    [Refactoring] Refactored table rendering with a state array + added a little of CSS

--- src/Game.js
+++ src/Game.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React,  { useState }  from 'react';
+import "./Game.css";
 
 const Game = () => {
+
+
+    const [positions, setPositions] = useState([-1,-1,-1,-1,-1,-1,-1,-1,-1]);
+
+
     return <div className="Game">
 
-        <table>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+        <table className="board">
+            <tbody>
+                {(() => {
+                    let rows = [];
+                    for (let y = 0; y < 3; y++) (
+                        rows.push(
+                            <tr key={y}>
+                                {positions.slice(y*3,y*3+3).map((val, x) =>
+                                    <td key={x}>
+                                    </td>
+                                )}
+                            </tr>
+                        )
+                    );
+                    return rows;
+                })()}
+            </tbody>
         </table>
         <div className="message">
 
