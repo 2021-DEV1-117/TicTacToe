5707aaae69e6186f03e448b34b3d6def022611d5
Your Name <youremail@yourdomain.com>
2021-11-28 16:36:12 +0100

    [Refactoring] Refactored the last test as similars tests will run to check others win possibilities.

--- src/Game.test.js
+++ src/Game.test.js
@@ -59,35 +59,33 @@ describe ('Game', () => {
         }
     });
 
+
+    const simulateGame = (wrapper,moves) => {
+        moves.forEach((cellIndex) => {
+            wrapper.find('td').at(cellIndex).simulate('click');
+        });
+    };
+
     it('If a player fill the first row, the game is done and a win message appear', () => {
         // CASE IF X fills the row
-
         let wrapper = shallow(<Game />);
-        wrapper.find('td').at(0).simulate('click'); // X  WIN
-        wrapper.find('td').at(3).simulate('click');
-        wrapper.find('td').at(1).simulate('click'); // X  WIN
-        wrapper.find('td').at(4).simulate('click');
-        wrapper.find('td').at(2).simulate('click'); // X  WIN
+        let moves = [0,3,1,4,2,8];
+        simulateGame(wrapper,moves);
 
-        wrapper.find('td').at(8).simulate('click');
         expect(wrapper.find('td').at(8).text()).toEqual('');
         expect(wrapper.find('div.message').first().text()).toEqual('X won the game');
 
         // CASE IF O fills the row
 
         wrapper = shallow(<Game />);
+        moves = [3,0,4,1,8,2,7];
+        simulateGame(wrapper,moves);
 
-        wrapper.find('td').at(3).simulate('click');
-        wrapper.find('td').at(0).simulate('click'); // O WIN
-        wrapper.find('td').at(4).simulate('click');
-        wrapper.find('td').at(1).simulate('click'); // O WIN
-        wrapper.find('td').at(8).simulate('click');
-        wrapper.find('td').at(2).simulate('click'); // O  WIN
-
-        wrapper.find('td').at(7).simulate('click');
         expect(wrapper.find('td').at(7).text()).toEqual('');
         expect(wrapper.find('div.message').first().text()).toEqual('O won the game');
 
     });
+
+
 
 });
