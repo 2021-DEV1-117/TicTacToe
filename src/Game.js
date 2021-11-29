90e674ffd96dfc4f5c9bb7648e09adec8e228325
Your Name <youremail@yourdomain.com>
2021-11-29 02:19:35 +0100

    [Refactoring] Refactoring + better variable names using consts

--- src/Game.js
+++ src/Game.js
@@ -3,17 +3,25 @@ import "./Game.css";
 
 const Game = ({ onCellClick }) => {
 
+    const PLAYER_X = 1;
+    const PLAYER_O = 0;
+    const PLAYER_NONE = -1;
+    
 
-    const [positions, setPositions] = useState([-1,-1,-1,-1,-1,-1,-1,-1,-1]);
+    const [positions, setPositions] = useState([
+        PLAYER_NONE,PLAYER_NONE,PLAYER_NONE,
+        PLAYER_NONE,PLAYER_NONE,PLAYER_NONE,
+        PLAYER_NONE,PLAYER_NONE,PLAYER_NONE
+    ]);
     const [history, setHistory] = useState([]);
-    const [winner, setWinner] = useState(-1);
+    const [winner, setWinner] = useState(PLAYER_NONE);
 
     const onClick = (position) => {
-        if (positions[position] === -1 && winner === -1) {
+        if (positions[position] === PLAYER_NONE && winner === PLAYER_NONE) {
 
-            let nextMove = 1;
+            let nextMove = PLAYER_X;
             if (history.length > 0) {
-                nextMove = history[history.length - 1] ? 0 : 1;
+                nextMove = history[history.length - 1] ? PLAYER_O : PLAYER_X;
             }
             setHistory([...history, nextMove]);
             let updatedPositions = [...positions];
@@ -36,10 +44,10 @@ const Game = ({ onCellClick }) => {
             [0,4,8],
             [2,4,6],
         ];
-        let victory = -1;
+        let victory = PLAYER_NONE;
 
         victoryPositions.forEach((victoryCheck) => {
-            if (positions[victoryCheck[0]] !== -1) {
+            if (positions[victoryCheck[0]] !== PLAYER_NONE) {
                 if (positions[victoryCheck[0]] === positions[victoryCheck[1]] && positions[victoryCheck[1]] === positions[victoryCheck[2]]) {
                     victory = positions[victoryCheck[0]];
                 }
@@ -52,12 +60,12 @@ const Game = ({ onCellClick }) => {
     const isDraw = () => {
         let gameFull = true;
         positions.forEach((pos) => {
-            if (pos === -1) {
+            if (pos === PLAYER_NONE) {
                 gameFull = false;
             }
         });
 
-        return gameFull && winner === -1;
+        return gameFull && winner === PLAYER_NONE;
     };
 
     return <div className="Game">
@@ -71,7 +79,7 @@ const Game = ({ onCellClick }) => {
                             <tr key={y}>
                                 {positions.slice(y*3,y*3+3).map((val, x) =>
                                     <td  onClick={()=> {onClick( y*3+x)}} key={x}>
-                                        { val === 1 ? 'X' : val === 0 ? 'O' : '' }
+                                        { val === PLAYER_X ? 'X' : val === PLAYER_O ? 'O' : '' }
                                     </td>
                                 )}
                             </tr>
@@ -82,8 +90,7 @@ const Game = ({ onCellClick }) => {
             </tbody>
         </table>
         <div className="message">
-            {winner === 0 ? 'O won the game' : winner === 1 ? 'X won the game' : ''}
-            {isDraw() ? 'Draw game' : ''}
+            {winner === PLAYER_O ? 'O won the game' : winner === PLAYER_X ? 'X won the game' : isDraw() ? 'Draw game' : ''}
         </div>
     </div>;
 }
